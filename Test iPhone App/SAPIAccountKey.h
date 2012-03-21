e45bba6c38f5fdf8897226c25229051dec3a69a4
Mark Aufflick <mark@aufflick.com>
2012-03-21 16:38:35 +1100

    added apikey header for test project

---
+++ Test iPhone App/SAPIAccountKey.h
@@ -0,0 +1,12 @@
+//
+//  SAPIAccountKey.h
+//  Sensis SAPI
+//
+//  Created by Mark Aufflick on 21/03/12.
+//  Copyright (c) 2012 Pumptheory Pty Ltd. All rights reserved.
+//
+
+#import "SAPI.h"
+
+#define SAPI_KEY @"put_your_sapi_key_here"
+#define SAPI_ENVIRONMENT SAPIEnvironmentTest
