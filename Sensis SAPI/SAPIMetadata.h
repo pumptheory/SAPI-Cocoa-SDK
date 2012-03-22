21e1b1a6118376635f316a0c93224fd062bff37b
Mark Aufflick <mark@aufflick.com>
2012-03-22 20:57:25 +1100

    moved some files

---
+++ Sensis SAPI/SAPIMetadata.h
@@ -0,0 +1,13 @@
+//
+//  SAPIMetadata.h
+//  Sensis SAPI
+//
+//  Created by Mark Aufflick on 22/03/12.
+//  Copyright (c) 2012 Pumptheory Pty Ltd. All rights reserved.
+//
+
+#import "SAPIEndpoint.h"
+
+@interface SAPIMetadata : SAPIEndpoint
+
+@end
---
+++ Sensis SAPI/SAPIMetadata.m
@@ -0,0 +1,13 @@
+//
+//  SAPIMetadata.m
+//  Sensis SAPI
+//
+//  Created by Mark Aufflick on 22/03/12.
+//  Copyright (c) 2012 Pumptheory Pty Ltd. All rights reserved.
+//
+
+#import "SAPIMetadata.h"
+
+@implementation SAPIMetadata
+
+@end
